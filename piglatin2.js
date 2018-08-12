031e40c16790664e5c36e6dc2a9735489ee560c8
AJ Magracia <ajmagracia@gmail.com>
2018-08-12 10:56:55 -0700

    Added comments to the code

--- piglatin2.js
+++ piglatin2.js
@@ -1,4 +1,4 @@
-//TODO: Get it to work
+//TODO: functionality for words in all capitals to retain the capitalization
 
 function pigLatin() {
   var string = document.getElementById('input').value;
@@ -30,36 +30,58 @@ function pigLatin() {
     //return the string for use outside function
     return joined
   }
+
   //This function checks if a string begins or ends with punctuation, removes said punctuation (if any), and stores said punctuation (if any) to put it back after transformation
   function checkPunctuation(string) {
+    //this regex looks for the first alphanumeric (basically non symbol)
     let letterStart = /\w/
+    //this looks for any amount of symbols at the end of a word
     let endPunct = /\W*$/
+    //gives the index of the first letter (if any)
     let testLetter = string.search(letterStart)
+    //slices from the index to the end (removing all beginning non-alphanumerics)
     let noStartPunct = string.slice(testLetter)
+    //slices from the beginning to the index (keeping only beginning non-alphanumerics)
     keepStart = string.slice(0, testLetter)
+    //test for end symbols on the string with its beginning symbols removed and returns index of the first end symbol
     let testEndPunct = noStartPunct.search(endPunct)
+    //slices from the beginning to the index (all that is left now is alphanumerics)
     let noPunct = noStartPunct.slice(0, testEndPunct)
+    //slices from the index to the end (keeping the end symbols)
     keepEnd = noStartPunct.slice(testEndPunct)
+    //returns the remaining letters-only string
     return noPunct
   }
 
+  //this function checks which pig latin rule to apply and applies it
   function transform(string) {
+    //this regex looks for all vowels, capitalized or not
     let vowels = /[aeiouyAEIOUY]/
+    //gives the index of the first vowel
     let firstVowel = string.search(vowels)
+    //if the first vowel is a y and it is followed by a vowel, slice after the y, swap positions, and add 'ay'
     if (/^[yY][aeiou]+/.test(string)) {
       var transformed = string.slice(firstVowel + 1).concat(string.slice(0, firstVowel + 1)).concat('ay')
+      //if the first vowel is the first letter (including y followed by a consonant, e.g. 'Yvette'), just add 'way'
     } else if (firstVowel === 0) {
       var transformed = string.concat('way')
+      //if the first vowel is a u and the preceeding letter is a q (both not case-sensitive), slice after the u, swap positions, and add 'ay'
     } else if (/[uU]/.test(string[firstVowel]) && /[qQ]/.test(string[firstVowel - 1])) {
       var transformed = string.slice(firstVowel + 1).concat(string.slice(0, firstVowel + 1)).concat('ay')
+      //if there is no letter in the string or if the string contains a number, do nothing
+      //personal preference because I think applying Pig Latin to '11am' or 'C3PO' looks ridiculous
     } else if (!/[a-zA-z]/.test(string) || /\d/.test(string)) {
       var transformed = string
+      //otherwise just slice until the vowel, swap positions, and add 'ay'
     } else {
       var transformed = string.slice(firstVowel).concat(string.slice(0, firstVowel)).concat('ay')
     }
+    //return the result of the transformation
     return transformed
   }
 
+  //this function takes a string and a boolean set previously (which noted whether the word was originally capitalized or not), and recapitalizes
+  //the code should be self-explanitory
   function fixCaps(string, wasCap) {
     if (wasCap) {
       let lower = string.toLowerCase()
@@ -72,15 +94,18 @@ function pigLatin() {
     return fixedCaps
   }
 
+  //this function simply returns the punctuation that was removed earlier
   function fixPunct(string) {
     return keepStart.concat(string).concat(keepEnd)
   }
-  //NOTE: THIS IS THE FUNCTION CALL
+
+  //This is the "real" function call per se
   var result = pigLatined(splitString)
   // console.log(result)
   //NOTE: Method 1
   // window.alert(result)
   //NOTE: Method 2, see html
+  //displays the result in the text box on the webpage
   document.getElementById('output').value = result;
 
 }
